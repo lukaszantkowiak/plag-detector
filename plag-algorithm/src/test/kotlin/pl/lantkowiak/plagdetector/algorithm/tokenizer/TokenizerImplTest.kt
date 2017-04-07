974aa190d5de752eb5e232739cc5f433998d1ab9
Lukasz Antkowiak <lukasz.patryk.antkowiak@gmail.com>
2017-04-07 19:56:39 +0200

    test for TokenizerImpl

---
+++ plag-algorithm/src/test/kotlin/pl/lantkowiak/plagdetector/algorithm/tokenizer/TokenizerImplTest.kt
@@ -0,0 +1,29 @@
+package pl.lantkowiak.plagdetector.algorithm.tokenizer
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import pl.lantkowiak.plagdetector.algorithm.tokenizer.lexer.LexerType
+import java.nio.file.Files
+import java.nio.file.Paths
+
+class TokenizerImplTest {
+    private val INPUT_FILE = "tokenizer/HelloWorld.java"
+    private val RESULT_FILE = "tokenizer/result.txt"
+
+    private val tokenizer: TokenizerImpl = TokenizerImpl()
+
+    @Test
+    fun tokenize_should_tokenizeCode_when_passed() {
+        // given
+        val inputResource = Paths.get(javaClass.classLoader.getResource(INPUT_FILE).toURI())
+        val input = Files.lines(inputResource).reduce { a, b -> a + " " + b }.get()
+
+        // when
+        val result = tokenizer.tokenize(LexerType.JAVA_8, input)
+
+        // then
+        val expectedResource = Paths.get(javaClass.classLoader.getResource(RESULT_FILE).toURI())
+        val expected = Files.lines(expectedResource).findFirst().get().split(", ").map { i -> Integer.valueOf(i) }
+        assertThat(result).containsExactlyElementsOf(expected);
+    }
+}
---
+++ plag-algorithm/src/test/resources/tokenizer/HelloWorld.java
@@ -0,0 +1,7 @@
+package test;
+
+public class HelloWorld {
+    public static void main(final String[] args) {
+        System.out.println("Hello World");
+    }
+}
