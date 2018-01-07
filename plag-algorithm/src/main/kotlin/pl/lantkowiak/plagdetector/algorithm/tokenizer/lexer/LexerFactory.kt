bf6f979e6b90322b83f90c42d79b65a04de35246
Lukasz Antkowiak <lukasz.antkowiak@siili.com>
2018-01-07 22:37:59 +0100

    add a few grammars

--- plag-algorithm/src/main/kotlin/pl/lantkowiak/plagdetector/algorithm/tokenizer/lexer/LexerFactory.kt
+++ plag-algorithm/src/main/kotlin/pl/lantkowiak/plagdetector/algorithm/tokenizer/lexer/LexerFactory.kt
@@ -2,13 +2,18 @@ package pl.lantkowiak.plagdetector.algorithm.tokenizer.lexer
 
 import org.antlr.v4.runtime.Lexer
 import org.antlr.v4.runtime.CharStream
-import pl.lantkowiak.plagdetector.algorithm.grammar.Java8Lexer
+import pl.lantkowiak.plagdetector.algorithm.grammar.*
 
 class LexerFactory {
     companion object {
         fun getLexer(type: LexerType, cs: CharStream): Lexer {
             when (type) {
                 LexerType.JAVA_8 -> return Java8Lexer(cs)
+                LexerType.CPP -> return CPP14Lexer(cs)
+                LexerType.C -> return CLexer(cs)
+                LexerType.PHP -> return PhpLexer(cs)
+                LexerType.KOTLIN -> return KotlinLexer(cs)
+                LexerType.PASCAL -> return pascalLexer(cs)
             }
         }
     }
--- plag-algorithm/src/main/kotlin/pl/lantkowiak/plagdetector/algorithm/tokenizer/lexer/LexerType.kt
+++ plag-algorithm/src/main/kotlin/pl/lantkowiak/plagdetector/algorithm/tokenizer/lexer/LexerType.kt
@@ -1,7 +1,12 @@
 package pl.lantkowiak.plagdetector.algorithm.tokenizer.lexer
 
 enum class LexerType(val title: String) {
-    JAVA_8("Java 8");
+    JAVA_8("Java 8"),
+    C("C"),
+    CPP("C++"),
+    KOTLIN("Kotlin"),
+    PASCAL("Pascal"),
+    PHP("PHP");
 
     companion object {
         fun getByTitle(title: String): LexerType {
